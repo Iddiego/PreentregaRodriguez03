90d8aa938cac9b3c611bf516352d0c4742baefb9
Iddiego <131209615+Iddiego@users.noreply.github.com>
2023-07-24 21:20:20 -0500

    03

---
+++ Js/planes.js
@@ -0,0 +1,57 @@
+const planes = [ 
+    
+    
+{
+    nombre: 'Día',
+    descripcion: 'Accedes a una sede especial por maximo 3 horas',
+    valor: 15
+},
+
+{
+    nombre: 'Mensual',
+    descripcion: 'Accedes a tu sede todo el mes sin limite de tiempo por dìa',
+    valor: 60
+},
+
+{
+    nombre: 'Bimensual',
+    descripcion: 'Accedes a tu sede dos meses sin limite de tiempo por dìa',
+    valor: 100
+},
+
+{
+    nombre: 'Semestral',
+    descripcion: 'Accedes a tu sede por 6 meses',
+    valor: 160
+},
+
+{
+    nombre: 'Anual',
+    descripcion: 'Accedes a tu sede por todo el año',
+    valor: 700
+},
+
+{
+    nombre: 'Anual-Especial',
+    descripcion: 'Accedes a cualquier sede con entrenador personal por todo el año',
+    valor: 820
+},
+
+{
+    nombre: 'Mensual-Especial',
+    descripcion: 'Accedes a tu sede todo el mes sin limite de tiempo por dìa y con entrenador personal',
+    valor: 95
+},
+
+{
+    nombre: 'Beast-Mode',
+    descripcion: 'Año y medio con seguimiento de entrenador y nutricionista',
+    valor: 1250
+}
+
+
+
+
+
+
+]
